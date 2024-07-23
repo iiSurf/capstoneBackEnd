4e8bdff3f0f2bae247a04d6a1a311e897fbee806
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-23 15:25:38 -0700

    updated config/db mjs folder and is now done

---
+++ config/db.mjs
@@ -0,0 +1,21 @@
+// Imports
+import mongoose from 'mongoose';
+import  dotenv from 'dotenv';
+
+// Set Up
+dotenv.config();
+const db = process.env.MONGO_URI;
+
+// Connection Function
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+        console.log(`Mongoose DB Connected...`);
+    
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
